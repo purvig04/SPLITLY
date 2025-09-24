29a20eefff8996bb8b41e244bdda0ac259f76319
purvi <purvig@id.argusoft.com>
2025-09-24 15:05:30 +0530

    add login page

---
+++ client/src/components/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login">
+        <form>
+    
+            <h2>Login Page</h2>
+    
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" />
+
+    
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" />
+    
+            <button type="button" @click="login">Login</button>
+    
+            <p>
+                Don't have an account?
+                <router-link to="/register">Register</router-link>
+            </p>
+        </form>
+    </div>
+    </template>
+    
+    <script>
+    
+    export default {
+        name: "LoginPage",
+        data() {
+            return {
+                email: "",
+                password: "",
+                // errors:{}
+            };
+        },
+ 
+    };
+    </script>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from "vue-router";
 import RegisterPage from "./components/RegisterPage.vue";
+import LoginPage from "./components/LoginPage.vue";
 
 const routes = [
   { name: "Register", path: "/register", component: RegisterPage },
+  { name: "Login", path: "/login", component: LoginPage },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });

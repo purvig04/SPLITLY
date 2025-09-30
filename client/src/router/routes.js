3b46c7882b76b240b6a76e58dbd9f9b4b2f6c4f7
purvi <purvig@id.argusoft.com>
2025-09-30 18:01:57 +0530

    change signup page name

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,5 +1,5 @@
 import { createWebHistory, createRouter } from "vue-router";
-import RegisterPage from "../views/SignUp/SignUpPage.vue";
+import RegisterPage from "../views/Register/RegisterPage.vue";
 import LoginPage from "../views/Login/LoginPage.vue";
 import HomePage from "../views/Home/HomePage.vue";
 import LandingPage from "../views/Landing/LandingPage.vue";
@@ -7,8 +7,8 @@ import LandingPage from "../views/Landing/LandingPage.vue";
 const routes = [
   { name: "Register", path: "/register", component: RegisterPage },
   { name: "Login", path: "/login", component: LoginPage },
-  { name: "Home", path: "/", component: HomePage },
-  { name: "Landing", path: "/landing", component: LandingPage },
+  { name: "Home", path: "/home", component: HomePage },
+  { name: "Landing", path: "/", component: LandingPage },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });

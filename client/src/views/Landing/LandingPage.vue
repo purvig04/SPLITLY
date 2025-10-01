42c5cb4e2375ef98764775d61a24bd8ee8541a92
purvi <purvig@id.argusoft.com>
2025-10-01 10:59:16 +0530

    added comment in landing

--- client/src/views/Landing/LandingPage.vue
+++ client/src/views/Landing/LandingPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="main">
-  <!-- Full-page container that centers its content -->
+  <!-- Full-page container  -->
   <div class="d-flex align-items-center justify-content-center vh-100 p-3">
     <!-- Landing Section -->
     <section id="landing" class="landing-section">
       <div class="container">
         <div class="row align-items-center">
-          <!-- Left Content: Headline + CTA -->
+          <!-- Left Content -->
           <div class="col-md-6 intros text-white mb-4 mb-md-0">
             <h1 class="landing-title mb-4">
               <span>Expenses made simple.</span><br />
@@ -18,7 +18,7 @@
               Add Expenses <span><i class="fa-solid fa-plus"></i></span>
             </button>
           </div>
-          <!-- Right Content: Illustration -->
+          <!-- Right Content-->
           <div class="col-md-6 text-center text-md-end">
             <img
               src="../../assets/images/illustrations/landing_payment_illustartion.svg"

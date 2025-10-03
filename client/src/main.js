fa53186826c600aaebac3cacbe46aad9b8824553
purvi <purvig@id.argusoft.com>
2025-10-03 10:16:12 +0530

    resolved merge conflicts

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,12 @@ import apolloClient from "./apollo";
 import App from "./App.vue";
 import router from "./router/routes";
 import store from "./store"; 
+import apolloClient from "./services/apollo";
+import { DefaultApolloClient } from "@vue/apollo-composable/dist";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import "bootstrap";
 
 const app = createApp(App);
 
@@ -16,3 +22,8 @@ app
   .use(store)
   .use(router)
   .mount("#app");
+
+
+
+
+createApp(App).provide(DefaultApolloClient, apolloClient).use(router).mount("#app");
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,61 @@
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+const GET_USER = gql`
+  query GetUser {
+    getUser {
+      name
+    }
+  }
+`;
 
 export default {
-
   name: "HomePage",
-  methods:{
+  setup(){
+    const {result, loading, error} = useQuery(GET_USER)
+
+    return {result , loading,error}
     
-  }
+  },
+  data() {
+    return {
+      user: { name: "Purvi" }, // Replace with backend fetched value
+      balances: {
+        owedToYou: 2500, // from backend
+        youOwe: 1200, // from backend
+      },
+    };
+  },
+
+
+  methods: {
+    async fetchUserName() {return },
+    goToGroups() {
+      this.$router.push("/groups");
+    },
+    goToNonGroup() {
+      this.$router.push("/non-group");
+    },
+    goToGeneral() {
+      this.$router.push("/general");
+    },
+    goToAnalysis() {
+      this.$router.push("/analysis");
+    },
+    goToAddExpense() {
+      this.$router.push("/add-expense");
+    },
+  },
+  mounted() {
+    this.fetchUserName();
+
+    const storedUser = JSON.parse(localStorage.getItem("userLoggedIn"));
+    console.log(storedUser);
+
+    if (storedUser) {
+      this.$router.push("/home");
+    } else {
+      this.$router.push("/register");
+    }
+  },
 };

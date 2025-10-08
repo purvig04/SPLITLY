cdc112cd3b573952ddc4c0b55d7b156be9017e90
purvi <purvig@id.argusoft.com>
2025-10-08 18:27:38 +0530

    backend logic to get and create group

--- server/graphQL/resolvers.js
+++ server/graphQL/resolvers.js
@@ -14,6 +14,22 @@ export const resolvers = {
       }
       return context.user;
     },
+
+    getGroups(_, __, { prisma, user }) {
+      return prisma.group.findMany({
+        where: {
+          OR: [
+            { createdById: user.id },
+            { members: { some: { userId: user.id } } },
+          ],
+        },
+        include: {
+          members: {
+            include: { user: true },
+          },
+        },
+      });
+    },
   },
 
   Mutation: {
@@ -23,7 +39,7 @@ export const resolvers = {
         data: { name, email, password: hashedPassword, contact },
       });
 
-      return  user ;
+      return user;
     },
 
     async login(parent, { email, password }, context) {
@@ -39,7 +55,7 @@ export const resolvers = {
       const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
       res.cookie("jwt", token, {
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 2,
+        maxAge: 1000 * 60 * 60 * 24 * 1,
       });
       return { token, user };
     },
@@ -48,6 +64,43 @@ export const resolvers = {
       const { res } = context;
       res.clearCookie("jwt", { httpOnly: true });
       return true;
+    },
+
+    async createGroup(_, { title, type, members = [] }, { prisma, user }) {
+      const groupType = type || "GROUP";
+      const group = await prisma.group.create({
+        data: {
+          title,
+          type: groupType,
+          createdById: user.id,
+        },
+      });
+      if (members.length > 0) {
+        for (const member of members) {
+          let existingUser = null;
+
+          //for now input is email
+          existingUser = await prisma.user.findUnique({
+            where: { email: member },
+          });
+
+          if (existingUser) {
+            // User exists so add to group
+            await prisma.groupMember.create({
+              data: {
+                userId: existingUser.id,
+                groupId: group.id,
+              },
+            });
+          } else {
+            // User not found → send invite
+            console.log(`Send invite to ${member}`);
+            //will handle this later
+          }
+        }
+      }
+
+      return group;
     },
   },
 };
--- server/graphQL/schema.js
+++ server/graphQL/schema.js
@@ -7,15 +7,38 @@ export const typeDefs = `#graphql
         createdAt:String!
         updatedAt:String!
         }
-    
+
+    enum GroupType{
+        PERSONAL
+        NON_GROUP
+        GROUP
+    }
+
+    type Group {
+        id:ID!
+        title: String!
+        type:GroupType!
+        createdById : String!
+        members : [GroupMember!]!
+    }
+
+    type GroupMember {
+        id: ID!
+        user:User!
+        groupId:String!
+        joinedAt:String!
+    }
+
     type Query{
         getUser:User
+        getGroups:[Group!]!
     }
 
     type Mutation{
         register( name:String! password:String! email:String! contact:String! ):User!
         login(password:String! email:String! ):AuthPayload
         logout:Boolean!
+        createGroup(title:String!, type:GroupType,members:[String]):Group!
     }
 
     type AuthPayload {

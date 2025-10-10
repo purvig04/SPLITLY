e8f06b3461a039cf5396110416fae6b030c6d94e
purvi <purvig@id.argusoft.com>
2025-10-10 15:59:51 +0530

    add group link

--- client/src/router/mainLayout.routes.js
+++ client/src/router/mainLayout.routes.js
@@ -3,6 +3,8 @@ import HomePage from "../views/Home/HomePage.vue";
 import GroupPage from "@/views/Groups/GroupPage.vue";
 import FriendsPage from "@/views/Friends/FriendsPage.vue";
 // import ChatsPage from "@/views/Chats/ChatsPage.vue";
+import AllGroups from "@/views/AllGroups/AllGroups.vue";
+import GroupPage from "@/views/SingleGroup/GroupPage.vue";
 
 export default {
   path: "/home",
@@ -18,5 +20,8 @@ export default {
         // { name: "Chats", path: "chats/:friendId", component: ChatsPage },
       ],
     },
+    { name: "Groups", path: "/groups", component: AllGroups },
+    { name: "Group", path: "/group/:id", component: GroupPage ,props :true},
+    //   { name: "Friends", path: "/friends", component: FriendsPage },
   ],
 };

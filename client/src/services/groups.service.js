4fbe4e073d0bb730e8ebcb54f9c05e7ec8d5df9d
purvi <purvig@id.argusoft.com>
2025-10-15 10:22:51 +0530

    add member to group service

--- client/src/services/groups.service.js
+++ client/src/services/groups.service.js
@@ -5,6 +5,7 @@ const GET_GROUPS = gql`
   query GetGroups($type: String) {
     getGroups(type: $type) {
       title
+      id
     }
   }
 `;
@@ -14,6 +15,47 @@ const CREATE_GROUP_MUTATION = gql`
     createGroup(title: $title, type: $type) {
       type
       title
+    }
+  }
+`;
+
+const GET_GROUP_DETAILS = gql`
+  query getGroupDetails($id: ID!) {
+    getGroupDetails(id: $id) {
+      id
+      createdById
+      title
+      type
+      members {
+        user {
+          name
+          id
+          email
+        }
+      }
+    }
+  }
+`;
+
+const ADD_MEMBER_TO_GROUP = gql`
+  mutation AddMemberToGroup($groupId: String!, $emails: [String!]!) {
+    addMemberToGroup(groupId: $groupId, emails: $emails) {
+      added
+      alreadyMembers
+      invited
+      updatedGroup {
+        id
+        createdById
+        title
+        type
+        members {
+          user {
+            name
+            id
+            email
+          }
+        }
+      }
     }
   }
 `;
@@ -34,7 +76,28 @@ export const groupService = {
       variables: { title, type },
       refetchQueries: [{ query: GET_GROUPS, variables: { type } }],
       awaitRefetchQueries: true,
+      mutation: CREATE_GROUP_MUTATION,
+      variables: { title },
     });
-    return resp.data.createGroup;
+    return resp.data;
   },
+
+  async getGroupDetails(id) {
+    const resp = await apolloClient.query({
+      query: GET_GROUP_DETAILS,
+      variables: { id },
+      fetchPolicy: "network-only",
+    });
+    return resp.data;
+  },
+
+  async addMemberToGroup(groupId,emails){
+    const resp = await apolloClient.mutate({
+      mutation:ADD_MEMBER_TO_GROUP,
+      variables:{groupId , emails},
+      fetchPolicy:'no-cache',
+    })
+    return resp.data
+  }
+  
 };

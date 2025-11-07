ff4c4088e958e2b41c300844434b454e8d3eec1a
purvi <purvig@id.argusoft.com>
2025-11-07 12:35:39 +0530

    imported resolvers and typedefs

--- server/graphQL/resolvers/index.js
+++ server/graphQL/resolvers/index.js
@@ -2,10 +2,14 @@ import { friendsResolvers } from "./friends.resolver.js";
 import { groupResolvers } from "./groups.resolver.js";
 import { userResolvers } from "./user.resolver.js";
 import { chatResolvers } from "./chats.resolver.js";
+import { categoriesResolvers } from "./categories.resolver.js";
+import { expensesResolvers } from "./expenses.resolver.js";
 
 export const resolvers = [
   userResolvers,
   groupResolvers,
   friendsResolvers,
   chatResolvers,
+  categoriesResolvers,
+  expensesResolvers
 ];
--- server/graphQL/typeDefs/index.js
+++ server/graphQL/typeDefs/index.js
@@ -2,5 +2,14 @@ import { friendsTypeDefs } from "./friends.typeDef.js";
 import { groupTypeDefs } from "./groups.typeDef.js";
 import { userTypeDefs } from "./user.typeDef.js";
 import { chatTypeDefs } from "./chats.typeDef.js";
+import { expensesTypeDefs } from "./expenses.typeDef.js";
+import { categoriesTypeDefs } from "./categories.typeDef.js";
 
-export const typeDefs = [userTypeDefs, groupTypeDefs, friendsTypeDefs, chatTypeDefs];
+export const typeDefs = [
+  userTypeDefs,
+  groupTypeDefs,
+  friendsTypeDefs,
+  chatTypeDefs,
+  expensesTypeDefs,
+  categoriesTypeDefs
+];

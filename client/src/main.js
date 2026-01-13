02a84f81d77c0298474da306da05f5f9b891f980
Purvi Gupta <purvi.gupta@argusoft.com>
2026-01-13 10:36:59 +0530

    installed required packages in frontend

--- client/src/main.js
+++ client/src/main.js
@@ -8,8 +8,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import "bootstrap";
+import vue3GoogleLogin from "vue3-google-login";
 
 const app = createApp(App);
+app.use(vue3GoogleLogin, {
+  clientId: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+});
 
 app.provide(DefaultApolloClient, apolloClient).use(store).use(router);
 
